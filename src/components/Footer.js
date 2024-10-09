dbf808b0e5087f0e9632a309e5a605bbfbfe984f
shubham <ss.guwahati1996@gmail.com>
2024-10-09 22:57:23 +0530

    minor change to React project

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="text-center py-3">
-      <p>&copy; 2024 My Website. All Rights Reserved.</p>
+      <p>&copy; 2024 My Webpage. All Rights Reserved.</p>
     </footer>
   );
 };
